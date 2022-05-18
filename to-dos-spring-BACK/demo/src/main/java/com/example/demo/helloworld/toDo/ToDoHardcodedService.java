ee13e460518e9b8785ca8e224368415f3f966761
NachoMartin123 <nacho.mf95@gmail.com>
2022-05-18 11:50:56 +0200

    Working GET, POST, PUT, DELETE

---
+++ to-dos-spring-BACK/demo/src/main/java/com/example/demo/helloworld/toDo/ToDoHardcodedService.java
@@ -0,0 +1,54 @@
+package com.example.demo.helloworld.toDo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToDoHardcodedService {
+	
+	private static List<ToDo> toDos = new ArrayList();
+	private static int idCounter=0;
+	
+	
+	static {
+		toDos.add(new ToDo(++idCounter, "hola", "Learn to dance", new Date(), false));
+		toDos.add(new ToDo(++idCounter, "hola", "Learn Microservices", new Date(), false));
+		toDos.add(new ToDo(++idCounter, "hola", "Go to supermarket", new Date(), false));
+	}
+	
+	public List<ToDo> findAll(){
+		return toDos;
+	}
+	
+	public ToDo deleteById(long id) {
+		ToDo t = findById(id);
+		if(t==null)
+			return null;
+		if(toDos.remove(t))
+			return t;
+		return null;
+	}
+
+	public ToDo findById(long id) {
+		for(ToDo t : toDos) {
+			if(t.getId()==id)
+				return t;
+		}
+		return null;
+	}
+
+	public ToDo save(ToDo toDo) {
+		if(toDo.getId()==-1 || toDo.getId()==-0) {
+			toDo.setId(idCounter++);
+			toDos.add(toDo);
+		}else {
+			deleteById(toDo.getId());
+			toDos.add(toDo);
+		}
+		return toDo; 
+	}
+}
---
+++ to-dos-spring-BACK/demo/src/main/java/com/example/demo/helloworld/toDo/ToDoResource.java
@@ -0,0 +1,68 @@
+package com.example.demo.helloworld.toDo;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class ToDoResource {
+	
+	@Autowired
+	private ToDoHardcodedService todoService;
+	
+	@GetMapping("/users/{username}/toDos")
+	public List<ToDo> getAllToDos(@PathVariable String username){
+		return todoService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/toDos/{id}")
+	public ResponseEntity<Void> deleteToDo(@PathVariable String username, 
+			@PathVariable long id){
+		ToDo t = todoService.deleteById(id);
+		if(t!=null)
+			return ResponseEntity.noContent().build();
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/users/{username}/toDos/{id}")
+	public ToDo getToDo(@PathVariable String username, @PathVariable long id){
+		return todoService.findById(id);
+	}
+	
+	@PutMapping("/users/{username}/toDos/{id}")
+	public ResponseEntity<ToDo> updateToDo(@PathVariable String username, 
+			@PathVariable long id, @RequestBody ToDo t){
+		ToDo toDoUpdated = todoService.save(t);
+		if(t!=null)
+			return ResponseEntity.noContent().build();
+		return new ResponseEntity<ToDo>(toDoUpdated, HttpStatus.OK);
+	}
+	
+	@PostMapping("/users/{username}/toDos")
+	public ResponseEntity<Void> updateToDo(@PathVariable String username, @RequestBody ToDo t){
+		
+		ToDo toDoCreated = todoService.save(t);
+		
+		//coge la url y le añade al final el {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+			.buildAndExpand(toDoCreated.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();//returns uri of the created toDo
+	}
+	
+	
+
+}
